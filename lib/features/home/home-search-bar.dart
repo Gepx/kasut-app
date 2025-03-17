ef40e49d5a1c7b3dd59befdf2470f6da3a491f4b
egipsinargo <egipsinargo123@gmail.com>
2025-03-17 16:32:09 +0700

    redirect to search after click on input field

--- lib/features/home/home-search-bar.dart
+++ lib/features/home/home-search-bar.dart
@@ -1,32 +1,44 @@
 import 'package:flutter/material.dart';
+import '../search-page/search_item.dart';
 
 class HomeSearchBar extends StatelessWidget {
   const HomeSearchBar({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      height: 35,
-      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 2),
-      margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 2),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(5),
-        color: const Color.fromARGB(255, 239, 237, 237),
-      ),
-      child: TextField(
-        textAlign: TextAlign.left,
-        textAlignVertical: TextAlignVertical.center,
-        decoration: InputDecoration(
-          border: InputBorder.none,
-          hintText: '1.000.000+ authentic',
-          hintStyle: const TextStyle(color: Colors.grey, fontSize: 10),
-          contentPadding: EdgeInsets.zero,
-          isDense: true,
-          prefixIcon: const Icon(Icons.search, color: Colors.grey, size: 20),
-          prefixIconConstraints: const BoxConstraints(
-            minWidth: 30,
-            minHeight: 30,
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const SearchPage(autoFocus: true),
+          ),
+        );
+      },
+      child: Container(
+        alignment: Alignment.center,
+        height: 35,
+        padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 2),
+        margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 2),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5),
+          color: const Color.fromARGB(255, 239, 237, 237),
+        ),
+        child: TextField(
+          enabled: false,
+          textAlign: TextAlign.left,
+          textAlignVertical: TextAlignVertical.center,
+          decoration: InputDecoration(
+            border: InputBorder.none,
+            hintText: '1.000.000+ authentic',
+            hintStyle: const TextStyle(color: Colors.grey, fontSize: 10),
+            contentPadding: EdgeInsets.zero,
+            isDense: true,
+            prefixIcon: const Icon(Icons.search, color: Colors.grey, size: 20),
+            prefixIconConstraints: const BoxConstraints(
+              minWidth: 30,
+              minHeight: 30,
+            ),
           ),
         ),
       ),
