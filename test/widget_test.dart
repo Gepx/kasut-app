ec358c0b71758e73a9ef497026fdd3324f61cc7d
Alvin-Liandy <alvin.liandy@students.mikroskil.ac.id>
2025-04-13 16:41:02 +0700

    ignore todo warning and deprecated messages problems

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:tugasuts/main.dart';
+import 'package:kasut/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
